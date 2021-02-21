6d05d97350bc939d7dd6ebc8a34a5aa68627301c
xyc <xuyaochang@126.com>
2021-02-21 19:42:34 +0800

    添加两行sout代码

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("啊这!");
+        System.out.println("啊这!");
     }
 }
